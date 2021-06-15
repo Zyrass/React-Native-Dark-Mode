f0c462049544598ae5ee850a13a4d29d29b4febd
zyrass <zyrass@outlook.fr>
2021-06-15 07:50:53 +0200

    First commit
    Conception d'une 1ère application avec React Native

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Image, Button } from 'react-native';
 
 export default function App() {
+
+  // State
+  const [theme, setTheme] = useState(true)
+  
+  // functions
+  const switchClickedHandler = () => setTheme( currentTheme => !currentTheme )
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+      style = {{ 
+        ...styles.container,
+        backgroundColor: theme
+          ? "#f9f900"
+          : "#1f1f1f",
+      }}
+    >
+      <Image 
+        source = { 
+          theme 
+            ? require("./assets/images/sun.png") 
+            : require("./assets/images/moon.png")
+        }
+        style = {{
+          width: 350,
+          height: 350,
+          marginBottom: 30
+        }}
+      />
+      <Button 
+        title={ 
+          theme
+            ? "Bonjour - React Native"
+            : "Bonne nuit - React Native"
+        }
+        onPress={switchClickedHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +47,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
